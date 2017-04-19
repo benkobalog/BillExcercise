f0bacf1233b504e9fc960358cd152b7e3aeded45
Your Name <benko.balog@gmail.com>
2017-04-19 19:54:26 +0200

    Added test stubs for Service Charge

---
+++ src/test/scala/ServiceChargeSuite.scala
@@ -0,0 +1,22 @@
+import Bill._
+import org.scalatest._
+class ServiceChargeSuite extends FlatSpec with Matchers {
+
+  "A Bill with Service Charge" should "return 3.5 for cola coffee and cheese sandwich" in {
+    val orders = List("Cola", "Coffee", "Cheese Sandwich")
+    val value = Bill(orders)
+    value should be (3.5)
+  }
+
+  it should "return 0 for empty List" in {
+    val orders = List[String]()
+    val value = Bill(orders)
+    value should be(0.0)
+  }
+
+  it should "handle non existent items" in {
+    val orders = List("Pug family", "Cola")
+    val value = Bill(orders)
+    value should be(0.5)
+  }
+}
