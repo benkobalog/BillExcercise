7a42f0f8a823507e626924309af8f975004f3e95
Your Name <benko.balog@gmail.com>
2017-04-19 19:42:21 +0200

    Standard Bill ready, All test passed

--- src/main/scala/StandardBill.scala
+++ src/main/scala/StandardBill.scala
@@ -5,7 +5,11 @@ object StandardBill {
   val items = Map[String, Price]("Cola" -> 0.5, "Coffee" -> 1.0, "Cheese Sandwich" -> 2.0, "Steak Sandwhich" -> 4.5)
 
   def apply(orders: List[String]): Double = {
-    0.0
+    orders.foldLeft(0.0) {
+      (sum, item) =>
+        val price = items.getOrElse(item, 0.0)
+        price + sum
+    }
   }
 }
 
