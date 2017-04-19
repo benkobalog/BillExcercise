61ac01e90822f403fa2478eeaca050d390501947
Your Name <benko.balog@gmail.com>
2017-04-19 19:47:35 +0200

    Renaming class to Bill, added another function for service charge

--- src/test/scala/StandardBillSuite.scala
+++ src/test/scala/StandardBillSuite.scala
@@ -1,22 +1,22 @@
-import StandardBill._
+import Bill._
 import org.scalatest._
 class StandardBillSuite extends FlatSpec with Matchers {
 
   "A Standard Bill" should "return 3.5 for cola coffee and cheese sandwich" in {
     val orders = List("Cola", "Coffee", "Cheese Sandwich")
-    val value = StandardBill(orders)
+    val value = Bill(orders)
     value should be (3.5)
   }
 
   it should "return 0 for empty List" in {
     val orders = List[String]()
-    val value = StandardBill(orders)
+    val value = Bill(orders)
     value should be(0.0)
   }
 
   it should "handle non existent items" in {
     val orders = List("Pug family", "Cola")
-    val value = StandardBill(orders)
+    val value = Bill(orders)
     value should be(0.5)
   }
 }
