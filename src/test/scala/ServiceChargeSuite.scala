e4e1cf7757b61f5b0604305d78b8918751c0defe
Your Name <benko.balog@gmail.com>
2017-04-19 20:02:10 +0200

    Fill tests for Service Charge

--- src/test/scala/ServiceChargeSuite.scala
+++ src/test/scala/ServiceChargeSuite.scala
@@ -2,21 +2,29 @@ import Bill._
 import org.scalatest._
 class ServiceChargeSuite extends FlatSpec with Matchers {
 
-  "A Bill with Service Charge" should "return 3.5 for cola coffee and cheese sandwich" in {
+  "A Bill with Service Charge" should "return 3.5 * 1.1 for cola coffee and cheese sandwich" in {
     val orders = List("Cola", "Coffee", "Cheese Sandwich")
     val value = Bill(orders)
-    value should be (3.5)
+    value should be (3.5 * 1.1)
   }
 
-  it should "return 0 for empty List" in {
-    val orders = List[String]()
+  it should "work for only drinks" in {
+    val orders = List("Cola", "Coffee")
     val value = Bill(orders)
-    value should be(0.0)
+    value should be(1.5)
   }
 
-  it should "handle non existent items" in {
-    val orders = List("Pug family", "Cola")
+  it should "work for hot foods" in {
+    val orders = List("Steak Sandwhich", "Cola")
     val value = Bill(orders)
-    value should be(0.5)
+    value should be(5.0 * 1.2)
+  }
+
+  it should "limit the tip in 20" in {
+    val orders = List.fill(25)("Steak Sandwhich")
+    val value = Bill(orders)
+    // Tip would be 22.5 but it should only be 20
+    // Cost should be 135 by default
+    value should be(132.5)
   }
 }
