021ef2eca9857fdf287fbb92fbc4e80069ac0bc4
Your Name <benko.balog@gmail.com>
2017-04-19 20:48:22 +0200

    Cleanup

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -9,24 +9,15 @@ object Bill {
       "Cheese Sandwich" -> (2.0, Food),
       "Steak Sandwhich" -> (4.5, HotFood))
 
-  def apply(orders: List[String]): Double = {
-    orders.foldLeft(0.0) {
-      (sum, item) =>
-        val price = items.getOrElse(item, (0.0, NonItem))
-        price._1 + sum
-    }
-  }
+  def apply(orders: List[String]): Double = calculateBaseSum(orders)
 
   def withServiceCharge(orders: List[String]): Double = {
-    val baseSum = orders.foldLeft(0.0) {
-      (sum, item) =>
-        val price = items.getOrElse(item, (0.0, NonItem))
-        price._1 + sum
-    }
+    val baseSum = calculateBaseSum(orders)
     val categoryList =
       orders
-        .map(order => items.getOrElse(order, (0.0, NonItem)))
+        .map(getItem)
         .map(x => x._2)
+
     val multiplier =
       if (categoryList.contains(HotFood)) 1.2
       else if (categoryList.contains(Food)) 1.1
@@ -35,6 +26,17 @@ object Bill {
     if ((multiplier - 1.0) * baseSum >= 20) 20 + baseSum
     else multiplier * baseSum
   }
+
+  private def calculateBaseSum(orders: List[String]): Double =
+    orders.foldLeft(0.0) {
+    (sum, item) =>
+      val price = getItem(item)
+      price._1 + sum
+  }
+
+  private def getItem(item: String): (Price, Category) =
+    items.getOrElse(item, (0.0, NonItem))
+
 }
 
 sealed trait Category
